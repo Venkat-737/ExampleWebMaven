2694a0dc7af27f31ca895530fb086f8f334f0637
Venkat-737 <venkatsrisai670@gmail.com>
2025-01-17 19:43:04 +0530

    Example Maven Web

---
+++ src/main/java/com/kmit/ExampleMavenWeb/App.java
@@ -0,0 +1,13 @@
+package com.kmit.ExampleMavenWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
